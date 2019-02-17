7294cc1e7e6513d264fadf729a809cc02fdb9e93
fenici <fenici@github.com>
2019-02-17 15:42:09 +1100

    router added

---
+++ src/containers/CustomersListPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const CustomersListPage = () => <div>clp</div>;
+
+export default CustomersListPage;
---
+++ src/containers/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoginPage = () => <Container>login</Container>;
+
+export default LoginPage;
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  background: #fafafa;
+`;
