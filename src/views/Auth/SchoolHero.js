62fc89e33a144979df3f593a3aa8a6c33476f3ad
fenici <fenici@github.com>
2019-03-26 01:55:55 +1100

    [LD]add social media in landing

--- src/views/Auth/SchoolHero.js
+++ src/views/Auth/SchoolHero.js
@@ -2,17 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import SchoolHeroLayout from "./SchoolHeroLayout";
-import { Typography, Fab } from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
 import Button from "../../components/LpButton";
 import Link from "@material-ui/core/Link";
-
+import Facebook from '../../assets/facebook.png'
+import Twitter from '../../assets/twitter.png'
+import Ins from "../../assets/Ins.png"
 const backgroundImage =
-  "https://www.uts.edu.au/sites/default/files/sci-sci-gsh-building-architect-render-jonestreet.jpg";
+"https://www.uts.edu.au/sites/default/files/AlumniGreen_1.jpg"
 const styles = theme => ({
   background: {
         backgroundImage: `url(${backgroundImage})`,
     backgroundColor: "#ffffff", // Average color of the background image.
-    backgroundPosition: "center"
+    backgroundPosition: "center",
+    overflow:'Hidden'
   },
   button: {
     minWidth: 200
@@ -26,7 +29,27 @@ const styles = theme => ({
   },
   more: {
     marginTop: theme.spacing.unit * 2
-  }
+  },
+    icons: {
+        display: 'flex',
+    },
+    icon: {
+        // width: 38,
+        height: 38,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: theme.palette.warning.main,
+        marginRight: theme.spacing.unit,
+        marginTop:'15px',
+        '&:hover': {
+            // backgroundColor: "#649"
+        },
+
+    },
+    iconSize:{
+
+    }
 });
 
 function SchoolHero(props) {
@@ -61,6 +84,19 @@ function SchoolHero(props) {
       <Typography variant="body2" color="inherit" className={classes.more}>
         Discover the elite eduction
       </Typography>
+
+      <Grid item className={classes.icons}>
+        <a href="https://facebook.com/" className={classes.icon}>
+                  <img src={Facebook} alt="Facebook" className={classes.icon}/>
+        </a>
+        <a href="https://twitter.com/" className={classes.icon}>
+          <img src={Twitter} alt="Twitter" className={classes.icon} />
+        </a>
+              <a href="https://twitter.com/" className={classes.icon}>
+                  <img src={Ins} alt="Twitter" className={classes.icon} />
+              </a>
+          
+      </Grid>
     </SchoolHeroLayout>
   );
 }
