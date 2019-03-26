8c8ef84647a54221905b565a654827632e8ae7fe
fenici <fenici@github.com>
2019-03-26 21:11:46 +1100

    sample server and express setup

--- src/server/express.js
+++ src/server/express.js
@@ -1,6 +1,19 @@
 const express = require('express')
-const path = require('path');
 
 const app = express();
+const port = 3001
+
+//app.get(path, callback [, callback ...])
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 
+app.listen(port, (err)=>{
+    if(err){
+        console.err(err);
+        
+    }
+
+    console.info('Server started at port %s.', port)
+
+})
--- src/views/Auth/SchoolHero.js
+++ src/views/Auth/SchoolHero.js
@@ -5,17 +5,22 @@ import SchoolHeroLayout from "./SchoolHeroLayout";
 import { Typography, Grid } from "@material-ui/core";
 import Button from "../../components/LpButton";
 import Link from "@material-ui/core/Link";
-import Facebook from '../../assets/facebook.png'
-import Twitter from '../../assets/twitter.png'
-import Ins from "../../assets/Ins.png"
+import Facebook from "../../assets/facebook.png";
+import Twitter from "../../assets/twitter.png";
+import Ins from "../../assets/Ins.png";
+
+//Background image in landing page 
 const backgroundImage =
-"https://www.uts.edu.au/sites/default/files/AlumniGreen_1.jpg"
+  "https://www.uts.edu.au/sites/default/files/AlumniGreen_1.jpg";
+
+
+  //get the style down
 const styles = theme => ({
   background: {
-        backgroundImage: `url(${backgroundImage})`,
+    backgroundImage: `url(${backgroundImage})`,
     backgroundColor: "#ffffff", // Average color of the background image.
     backgroundPosition: "center",
-    overflow:'Hidden'
+    overflow: "Hidden"
   },
   button: {
     minWidth: 200
@@ -30,32 +35,31 @@ const styles = theme => ({
   more: {
     marginTop: theme.spacing.unit * 2
   },
-    icons: {
-        display: 'flex',
-    },
-    icon: {
-        // width: 38,
-        height: 38,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        // backgroundColor: theme.palette.warning.main,
-        marginRight: theme.spacing.unit,
-        marginTop:'15px',
-        '&:hover': {
-            // backgroundColor: "#649"
-        },
-
-    },
-    iconSize:{
-
+  icons: {
+    display: "flex"
+  },
+  icon: {
+    // width: 38,
+    height: 38,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: theme.palette.warning.main,
+    marginRight: theme.spacing.unit,
+    marginTop: "15px",
+    "&:hover": {
+      // backgroundColor: "#649"
     }
+  },
+  iconSize: {}
 });
 
 function SchoolHero(props) {
   const { classes } = props;
 
   return (
+
+    // background and overflowed text from school hero layout 
     <SchoolHeroLayout backgroundClassName={classes.background}>
       <img style={{ display: "none" }} src={backgroundImage} alt="" />
       <Typography color="inherit" align="center" variant="h2" marked="center">
@@ -85,17 +89,17 @@ function SchoolHero(props) {
         Discover the elite eduction
       </Typography>
 
+        {/* Social Media */}
       <Grid item className={classes.icons}>
         <a href="https://facebook.com/" className={classes.icon}>
-                  <img src={Facebook} alt="Facebook" className={classes.icon}/>
+          <img src={Facebook} alt="Facebook" className={classes.icon} />
         </a>
         <a href="https://twitter.com/" className={classes.icon}>
           <img src={Twitter} alt="Twitter" className={classes.icon} />
         </a>
-              <a href="https://twitter.com/" className={classes.icon}>
-                  <img src={Ins} alt="Twitter" className={classes.icon} />
-              </a>
-          
+        <a href="https://twitter.com/" className={classes.icon}>
+          <img src={Ins} alt="Twitter" className={classes.icon} />
+        </a>
       </Grid>
     </SchoolHeroLayout>
   );
